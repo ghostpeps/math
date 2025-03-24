c917cc3f7b33b55f4b6fff4a673fcb5c6b690459
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-23 21:05:01 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("π")
 st.write("π (paɪ) is a constant. π is the ratio of a circle's circumference to its diameter. π is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592 at 6:53 AM. The following picture shows the parts of a cricle and how to find the circumference and area.")
 st.image("circle.png")
-exp = ""
+exp = exp = r"\frac{1783366216531}{567663097408}"
 st.write(f"The exact value of π as a fraction is ${exp}$.")
