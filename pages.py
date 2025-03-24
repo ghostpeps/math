5135e3bf9f0febdc3900dc723da4c91f5da6785e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-24 07:49:21 -0700

    Update pages.py

--- pages.py
+++ pages.py
@@ -5,6 +5,6 @@ pgs = st.navigation([
   st.Page(page="pi.py", title="π"),
   st.Page(page="e.py", title="e"),
   st.Page(page="phi.py", title="Φ"),
-  st.Page(page="eipi.py", title=f"${exp}$")
+  st.Page(page="eipi.py", title="e<sup>iπ</sup>")
 ])
 pgs.run()
