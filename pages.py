f4fe103326c78851746dc7074aafd4e674865b85
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-23 21:08:19 -0700

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-latex_exp = "e^{i\\pi}"
 pgs = st.navigation([
   st.Page(page="pi.py", title="π"),
   st.Page(page="e.py", title="_e_"),
   st.Page(page="phi.py", title="Φ"),
-  st.Page(page="eipi.py", title=f"${latex_exp}$")
+  st.Page(page="eipi.py", title="e^iπ")
 ])
 pgs.run()
