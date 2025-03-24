0b3149c0a92029476b805422b7a4a3592a50b588
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-23 20:51:27 -0700

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("π")
+st.write("π (paɪ) is a constant. π is the ratio of a circle's circumference to its diameter. π is approximately equal to 3.14. π day is on March 14th becuase March is the 3rd month of the year and the 14th becuase it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592 at 6:53 AM. The following picture shows the parts of a cricle and how to find the circumference and area.")
+st.image("circle.png")
