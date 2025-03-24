59b46740a9babd6a531fb7abe1b1fc7f8fe22481
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-24 07:36:33 -0700

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,6 +4,6 @@ pgs = st.navigation([
   st.Page(page="pi.py", title="π"),
   st.Page(page="e.py", title="_e_"),
   st.Page(page="phi.py", title="Φ"),
-  st.Page(page="eipi.py", title="e^iπ")
+  st.Page(page="eipi.py", title="eⁱπ")
 ])
 pgs.run()
