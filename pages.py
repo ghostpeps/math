102603038dc4e9556fa1ce630a433c01e468c802
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-24 17:03:01 -0700

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,10 +1,23 @@
 import streamlit as st
 
-exp = r"e^{i\pi}"
+exp1 = r"e^{i\pi}"
+exp2 = r"\sqrt{2}"
 pgs = st.navigation([
   st.Page(page="pi.py", title="π"),
   st.Page(page="e.py", title="e"),
   st.Page(page="phi.py", title="Φ"),
-  st.Page(page="eipi.py", title="e<sup>iπ</sup>")
+  st.Page(page="eipi.py", title="e<sup>iπ</sup>"),
+  st.Page(page="sqrt2.py", title=exp2),
+  st.Page(page="i.py", title="i"),
+  st.Page(page="y.py", title="γ"),
+  st.Page(page="zeta3.py", title="ζ(3)"),
+  st.Page(page="g.py", title="G"),
+  st.Page(page="a.py", title="α"),
+  st.Page(page="d.py", title="δ"),
+  st.Page(page="omega.py", title="Ω"),
+  st.Page(page="h.py", title="h"),
+  st.Page(page="l.py", title="L"),
+  st.Page(page="c.py", title="c"),
+  st.Page(page="k.py", title="k")
 ])
 pgs.run()
