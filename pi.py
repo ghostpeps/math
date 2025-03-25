6c2b7c5513494f4f6bde9d64d01aedddb0fc689f
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-24 17:42:30 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -4,7 +4,7 @@ url = "https://www.cecm.sfu.ca/organics/papers/borwein/paper/html/local/bdigits.
 st.title("π")
 st.write("π (paɪ) is a constant. π is the ratio of a circle's circumference to its diameter. π is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592 at 6:53 AM. The following picture shows the parts of a cricle and how to find the circumference and area.")
 st.image("circle.png")
-exp = exp = r"\frac{1,783,366,216,531}{567,663,097,408}"
+exp = exp = r"\frac{1783366216531}{567663097408}"
 st.write(f"The exact value of π as a fraction is ${exp}$.")
 st.write("Here is a video to memorize the digits of π:")
 st.video(data="https://youtu.be/xsrJdSaiD9U?si=fXITLboT5BUW1I1k", loop=False)
