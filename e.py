fdc110598837416b8e3e1c9fc98c2680668e18c3
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-25 19:56:36 -0700

    Update e.py

--- e.py
+++ e.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("e")
 st.write("e (pronounced ē) also noun as Euler's number (pronounced **oy**·lrz nəmbər) is a constant that is at the base of a natural logarithm and exponential function. e is approximately equal to 2.71. e-day is on February 7th, while Ultimate e-day is on February 7th, 1828 at 6:28 PM. Below is how to solve for e.")
-st.latex("\displaystyle\lim_{x\rightarrow \infty} \left(1 + \frac{1}{n}\right)^{n}")
+st.latex("\lim_{n \to \infty} \left( 1 + \frac{1}{n} \right)^n")
