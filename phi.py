fec3e4c76ca00e36a998fc6da37ba50b001362ca
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-26 21:43:14 -0700

    Update phi.py

--- phi.py
+++ phi.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
+url = "https://sketchplanations.com/the-golden-ratio"
+caption = "Image Credit: [The Golden Ratio: Nature's Perfect Proportions - Sketchplanations](%s)" % url
 st.title("Φ")
 st.write("Φ (pronounced fē), also known as the golden ratio, two quantities are in the Φ if their ratio is the same as the ratio of their sum to the larger of the two quantities. Bellow is how to solve for Φ.")
 st.latex(r"\phi = \frac{1 + \sqrt{5}}{2}")
 st.write("Or")
 st.latex(r"\frac{a + b}{a} = \frac{a}{b} = \phi\ \ where\ \ a > b > 0")
+st.write("Bellow is a picture about Φ:")
+st.image(image="the-golden-ratio.png", caption=caption)
