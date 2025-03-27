b5b7e1c40b4f8cf06d085d5b9d6d1ab9524b6318
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-26 18:04:16 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 url = "https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt"
 st.title("π")
-st.write("π (pronounced paɪ) is a constant. π is the ratio of a circle's circumference to its diameter. π is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592, at 6:53 AM. The following picture shows the parts of a circle and how to find the circumference and area.")
+st.write("π (pronounced paɪ) is a constant. It is the ratio of a circle's circumference to its diameter. It is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592, at 6:53 AM. The following picture shows the parts of a circle and how to find the circumference and area.")
 st.image("circle.png")
 st.write("This is how to solve for π:")
 st.latex(r"\pi = \frac{C}{d}")
