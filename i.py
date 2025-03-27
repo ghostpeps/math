9e32f599f8f318ac6c18cdb4b73c7e8e50c3e3ed
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-27 07:29:38 -0700

    Update i.py

--- i.py
+++ i.py
@@ -4,4 +4,4 @@ i = r"i"
 sqrt = r"\sqrt{-1}"
 ipm = r"\pm i"
 st.title(f"${i}$")
-st.write(f"${i}$ is ${i}$. It is an extension to the Real Numbers (ℝ), making it a Complex Number (ℂ). So the answer to ${sqrt}$ is ${ipm}$.")
+st.write(f"${i}$ is ${sqrt}$. It is an extension to the Real Numbers (ℝ), making it a Complex Number (ℂ). So the answer to ${sqrt}$ is ${ipm}$.")
