1d0f657ba446ad5cefc04a3608df88260998905d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-27 07:21:29 -0700

    Update sqrt2.py

--- sqrt2.py
+++ sqrt2.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+sqrt2 = r"\sqrt{2}"
+frac2 = r"2^{\frac{1}{2}}"
+frac = r"\pm 1\frac{29}{70}"
+st.title(f"${sqrt2}$")
+st.write(f"${sqrt2}$ is equal to ${frac}$. It is an infinite decimal, approximately equal to 1.41. ${sqrt2}$ can also be written as ${frac2}$.")
