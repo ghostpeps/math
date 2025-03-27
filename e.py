b4765fbc4f2f32dfd24b87e90105ed7503b92fc3
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-26 17:04:05 -0700

    Update e.py

--- e.py
+++ e.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 url = "https://zacharyratliff.org/files/eMillionDigits.txt"
 st.title("𝑒")
-st.write("𝑒 (pronounced ē) also noun as Euler's number (pronounced **oy**·lrz nəmbər) is a constant that is at the base of a natural logarithm and exponential function. It is approximately equal to 2.71. 𝑒-day is on February 7th, while Ultimate 𝑒-day was on February 7th, 1828 at 6:28 PM. Below is how to solve for 𝑒.")
+st.write("𝑒 (pronounced ē) also known as Euler's number (pronounced **oy**·lrz nəmbər) is a constant that is at the base of a natural logarithm and exponential function. It is approximately equal to 2.71. 𝑒-day is on February 7th, while Ultimate 𝑒-day was on February 7th, 1828 at 6:28 PM. Below is how to solve for 𝑒.")
 st.latex(r"e = \lim_{n \to \infty} \left( 1 + \frac{1}{n} \right)^n")
 st.write("Or")
 st.latex(r"\sum_{n=0}^{\infty} \frac{1}{n!}")
