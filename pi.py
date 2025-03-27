46c8f6bfec6070eccc7e6c1db44463787cb6c22d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-26 18:03:33 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -5,7 +5,7 @@ st.title("π")
 st.write("π (paɪ) is a constant. π is the ratio of a circle's circumference to its diameter. π is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592, at 6:53 AM. The following picture shows the parts of a circle and how to find the circumference and area.")
 st.image("circle.png")
 st.write("This is how to solve for π:")
-st.latex(r"\pi = \frac{circumference}{diameter}")
+st.latex(r"\pi = \frac{C}{d}")
 exp = exp = r"3\frac{80376924307}{567663097408}"
 st.write(f"The exact value of π as a mixed number is ${exp}$.")
 st.write("Here is a video to memorize the digits of π:")
