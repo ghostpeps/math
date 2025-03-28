2bc72d536b6b8efee3ccbd7247fed857896f2991
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-28 08:42:04 -0700

    Update ii.py

--- ii.py
+++ ii.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 ii = r"i^i"
 neg_one = r"\sqrt{-1}^{\sqrt{-1}}"
+rad = r"\frac{1}{\sqrt{e^\pi}"
 st.title(f"${ii}$")
-st.write(f"${ii}$ is the same as saying ${neg_one}$.")
+st.write(f"${ii}$ is the same as saying ${neg_one}$. It is also the same as ${rad}$")
