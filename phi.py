a90abd2ea512dc617d6dc19129def0af2b200a0d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-28 08:16:31 -0700

    Update phi.py

--- phi.py
+++ phi.py
@@ -6,7 +6,7 @@ caption = "Image Credit: [The Golden Ratio: Nature's Perfect Proportions - Sketc
 st.title("Φ")
 st.write("Φ (pronounced fē), also known as the golden ratio, describes a relationship where two quantities are in Φ if their ratio corresponds to the ratio of their sum to the larger quantity. Bellow is how to solve for Φ.")
 st.latex(r"\phi = \frac{1 + \sqrt{5}}{2}")
-st.markdown("<div style="text-align: center"> Or </div>")
+st.markdown(<div style="text-align: center"> Or </div>)
 st.latex(r"\frac{a + b}{a} = \frac{a}{b} = \phi\ \ where\ \ a > b > 0")
 st.write("Bellow is a picture about Φ:")
 st.image(image="the-golden-ratio.png", caption=caption)
