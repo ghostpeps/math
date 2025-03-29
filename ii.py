dda23d62aa5e6789d4bd2e9ed37bf86f3000cf56
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-29 11:14:21 -0700

    Update ii.py

--- ii.py
+++ ii.py
@@ -5,4 +5,4 @@ neg_one = r"\sqrt{-1}^{\sqrt{-1}}"
 rad = r"\frac{1}{\sqrt{e^\pi}}"
 st.title(f"${ii}$")
 st.write(f"${ii}$ is the same as saying ${neg_one}$. It is also the same as ${rad}$. Bellow is how to solve for it.")
-st.latex(r"\[ i^i = \left(e{i\pi/2}\right)i = e^{i \cdot i \pi/2} = e^{-\pi/2} \] = e^{- \frac{\pi}{2}}")
+st.latex(r"i^i = \left(e^{i\frac{\pi}{2}}\right)^i = e^{i^2 \cdot \frac{\pi}{2}} = e^{-\frac{\pi}{2}}")
