0a14fb42f4ac105ef32c7476e372f12ac56e1eb2
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-29 11:26:34 -0700

    Update ii.py

--- ii.py
+++ ii.py
@@ -6,4 +6,4 @@ rad = r"\frac{1}{\sqrt{e^\pi}}"
 st.title(f"${ii}$")
 st.write(f"${ii}$ is the same as saying ${neg_one}$. It is also the same as ${rad}$. Bellow is how to solve for it.")
 st.latex(r"i^i = \left(e^{i\frac{\pi}{2}}\right)^i = e^{i^2 \cdot \frac{\pi}{2}} = e^{-\frac{\pi}{2}}")
-st.latex(r"i \approx 0.207879572")
+st.latex(r"i^i \approx 0.207879572")
