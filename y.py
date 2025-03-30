2eefaf44966f3d287463ff12d6785246d2785945
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-30 10:49:27 -0700

    Update y.py

--- y.py
+++ y.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+y = r"\gamma"
+st.title(f"${y}$")
+st.write(f"${y}$ (pronounced ɡamə), is approximately equal to 0.57721. It represents the Euler-Mascheroni constant. The definition of ${y}$ is:")
+st.latex(r"\gamma = \lim_{n \to \infty} \left( \sum_{k=1}^{n} \frac{1}{k} - \ln(n) \right)")
