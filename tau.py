448f62242d5ac2c75e70a2a46199a04554a38fb3
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 10:24:52 -0700

    Update tau.py

--- tau.py
+++ tau.py
@@ -5,6 +5,10 @@ st.title(f"${tau}$")
 st.write(f"${tau}$ (pronounced taʊ) is the double of π. That makes ${tau}$ approximately equal to 6.28. Bellow is a list of ways you could use it.")
 st.latex(r"C = d\tau")
 st.latex(r"A = \frac{\tau}{2} \cdot r^2")
-st.latex(r"e^{e\tau} = 1")
+st.latex(r"e^{i\tau} = 1")
 st.write(f"Bellow is hown to solve for ${tau}$:")
 st.latex(r"\tau = 2\pi")
+st.html(
+  """<p style="text-align: center;">Or</p>"""
+)
+st.latex(r"\tau = \frac{C}{r}")
