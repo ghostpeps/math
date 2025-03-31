407bb8e51c64b593337948162ee694a1b8749a7e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 10:05:12 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 url = "https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt"
-st.title("π")
-st.write("π (pronounced paɪ) is a constant. It is the ratio of a circle's circumference to its diameter. It is approximately equal to 3.14. π day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate π day was on March 14th, 1592, at 6:53 AM. The following picture shows the parts of a circle and how to find the circumference and area.")
+pi = r"\pi"
+st.title(f"${pi}$")
+st.write(f"${pi}$ (pronounced paɪ) is a constant. It is the ratio of a circle's circumference to its diameter. It is approximately equal to 3.14. ${pi}$ day is on March 14th because March is the 3rd month of the year and the 14th because it corresponds to the 14 in 3.14. Ultimate ${pi}$ day was on March 14th, 1592, at 6:53 AM. The following picture shows the parts of a circle and how to find the circumference and area.")
 st.image("circle.png")
-st.write("This is how to solve for π:")
+st.write(f"This is how to solve for ${pi}$:")
 st.latex(r"\pi = \frac{C}{d}")
 exp = exp = r"3\frac{80376924307}{567663097408}"
 st.write(f"The exact value of π as a mixed number is ${exp}$.")
-st.write("Here is a video to memorize the digits of π:")
+st.write(f"Here is a video to memorize the digits of ${pi}$:")
 st.video(data="https://youtu.be/xsrJdSaiD9U?si=fXITLboT5BUW1I1k", loop=False)
-st.write("Click [here](%s)" % url + " to see one billion digits of π.")
+st.write("Click [here](%s)" % url + " to see one billion digits of " + pi + ".")
