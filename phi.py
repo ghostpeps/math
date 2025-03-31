90eddc759c79f4ee151a521fc284ebc153be0ce4
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 10:11:12 -0700

    Update phi.py

--- phi.py
+++ phi.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 url = "https://sketchplanations.com/the-golden-ratio"
+phi = r"\phi"
 caption = "Image Credit: [The Golden Ratio: Nature's Perfect Proportions - Sketchplanations](%s)" % url
-st.title("Φ")
-st.write("Φ (pronounced fē), also known as the golden ratio, describes a relationship where two quantities are in Φ if their ratio corresponds to the ratio of their sum to the larger quantity. Bellow is how to solve for Φ.")
+st.title(f"${phi}$")
+st.write(f"${phi}$ (pronounced fē), also known as the golden ratio, describes a relationship where two quantities are in ${phi}$ if their ratio corresponds to the ratio of their sum to the larger quantity. Bellow is how to solve for it.")
 st.latex(r"\phi = \frac{1 + \sqrt{5}}{2}")
 st.html(
   """<p style="text-align: center;">Or</p>"""
 )
 st.latex(r"\frac{a + b}{a} = \frac{a}{b} = \phi\ \ where\ \ a > b > 0")
-st.write("Bellow is a picture about Φ:")
+st.write(f"Bellow is a picture about ${phi}$:")
 st.image(image="the-golden-ratio.png", caption=caption)
