081d4664f2b65f022b948a5b1f64916fc6ef98ff
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 12:25:20 -0700

    Update g.py

--- g.py
+++ g.py
@@ -4,9 +4,11 @@ g = r"G"
 st.title(f"${g}$")
 st.write(f"${g}$ is a proportionality constant used in Newton's law of universal gravitation to calculate the gravitational force between two objects. The formula is:")
 st.latex(r"F = G \cdot \frac{m_1 \cdot m_2}{r^2}")
-st.latex(r"\begin{itemize}")
-st.latex(r"\item F is the force of gravity")
-st.latex(r"\item m_1 is the first mass")
-st.latex(r"\item m_2 is the second mass")
-st.latex(r"\item r is the distance between the centers of m_1 and m_2")
-st.latex(r"\end{itemize}")
+st.latex(r"""
+\begin{itemize}
+\item F is the force of gravity
+\item m_1 is the first mass
+\item m_2 is the second mass
+\item r is the distance between the centers of m_1 and m_2
+\end{itemize}
+  """)
