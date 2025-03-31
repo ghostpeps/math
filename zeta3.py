569aad6b3aedaa8e582bcff56568f536081b63da
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 10:44:09 -0700

    Update zeta3.py

--- zeta3.py
+++ zeta3.py
@@ -3,7 +3,7 @@ import streamlit as st
 zeta = r"\zeta(3)"
 st.title(f"${zeta}$")
 st.write(f"${zeta}$, also known as Apéry's constant, is a constant using the zeta function. It can also be said as the zeta function at 3. Bellow is how to solve for ${zeta}$:")
-st.latex(r"\zeta(s) = \sum_{n=1}^{\infty} \frac{1}{n^s} for s > 1")
+st.latex(r"\zeta(s) = \sum_{n=1}^{\infty} \frac{1}{n^s}\ \ for\ \ s > 1")
 st.latex(r"Where\ \ s = 3")
 st.latex(r"\zeta(3) = \sum_{n=1}^{\infty} \frac{1}{n^3}")
 st.latex(r"\zeta(3) \approx 1.20205")
