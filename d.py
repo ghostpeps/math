fd79b97ea148268b7e98f476eb286275bc2ff518
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-01 09:27:48 -0700

    Update d.py

--- d.py
+++ d.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 delta = r"\delta"
 st.title(f"${delta}$")
+st.write(f"${delta}$ is used to show a small difference for a variable. Ex.")
+st.latex(r"\deltax = 2")
+st.write("This means that the change of $x$ is 2.")
