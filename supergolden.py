15e96472f449053494b009835aaf477b67149e68
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-01 10:53:35 -0700

    Update supergolden.py

--- supergolden.py
+++ supergolden.py
@@ -3,4 +3,5 @@ import streamlit as st
 golden = r"\psi"
 st.title(f"${golden}$")
 st.write(f"${golden}$ is a ratio. $a$ and $b$ are in it if:")
-st.latex(r"\left(\frac{a + b}{a}\right)^2 = \frac{a}{b} for a > b > 0")
+st.latex(r"\left(\frac{a + b}{a}\right)^2 = \frac{a}{b}\ \ for\ \ a > b > 0")
+st.latex(r"\psi = \sum_{n=0}{\infty} \psi^{-3n}")
