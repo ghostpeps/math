e8e1623cbd5184f01f37f4bc54a404c0e8d22921
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-31 23:54:12 -0700

    Update a.py

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import streamlit as st
 a = r"\alpha"
 frac = r"\frac{1}{137}"
 st.title(f"${a}$")
-st.write(f"${a}$ is a constant. It is mainly used when dealing with light and matter, particularly in quantum electrodynamics (QED). It is approximately equal to ${frac}}$. The formula is:")
+st.write(f"${a}$ is a constant. It is mainly used when dealing with light and matter, particularly in quantum electrodynamics (QED). It is approximately equal to ${frac}$. The formula is:")
 st.latex(r"\alpha = \frac{e^2}{2\tau\epsilon_0hc}")
 st.latex(r"Where:")
 st.latex(r"e = elementary\ \ charge")
